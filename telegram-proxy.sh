c1056836522d83582509b8fb4214cc17e580bf6c
vladobro87 <35462575+vladobro87@users.noreply.github.com>
2026-04-04 12:56:53 +0300

    Update telegram-proxy.sh

--- telegram-proxy.sh
+++ telegram-proxy.sh
@@ -22,7 +22,9 @@ DOMAIN="vk.com"		# Домен для маскировки TLS
 VERSION="3.3.36"				# Версия telemt (если версия не указана то ставим последнюю релизную версию)
 MAX_CONNECTIONS=2000	# Максимальное количество одновременных TCP подключений (0 = без лимита)
 BUF_DIVISOR=4			# Делитель буферов - уменьшает буферы если не хватает оперативной памяти (1-16, по умолчанию без деления = 1)
-
+echo "Переменные"
+echo "$DOMAIN --- $PORT --- $VERSION --- $MAX_CONNECTIONS --- $BUF_DIVISOR"
+echo
 # Максимум TCP подключений для 1Gb оперативной памяти:
 # MAX_CONNECTIONS=1000 при BUF_DIVISOR=1
 # MAX_CONNECTIONS=2000 при BUF_DIVISOR=4
@@ -55,12 +57,12 @@ BUF_S2C=$((262144 / BUF_DIVISOR))
 BUF_CRYPTO=$((262144 / BUF_DIVISOR))
 
 # Остановка сервиса
-echo Остановка сервиса
+echo "Остановка сервиса"
 
 systemctl disable --now telemt 2>/dev/null || true
 
 # Скачивание бинарника
-echo Скачивание бинарника
+echo "Скачивание бинарника"
 
 TMP_DIR="$(mktemp -d)"
 echo "Downloading $TELEMT_URL"
@@ -70,7 +72,7 @@ install -m 0755 "$TMP_DIR/telemt" "$BINARY_PATH"
 rm -rf "$TMP_DIR"
 
 # Инициализация конфигурации
-echo Инициализация конфигурации
+echo "Инициализация конфигурации"
 
 $BINARY_PATH --init \
 	--port "$PORT" \
@@ -81,13 +83,13 @@ $BINARY_PATH --init \
 	--no-start > /dev/null 2>&1
 
 # Отключение beobachten
-echo Отключение beobachten
+echo "Отключение beobachten"
 
 sed -i 's/^beobachten = .*/beobachten = false/' "$CONFIG_FILE"
 grep -q '^beobachten = false' "$CONFIG_FILE" || sed -i '/^\[general\]/a beobachten = false' "$CONFIG_FILE"
 
 # Отключение IPv6
-echo Отключение IPv6
+echo "Отключение IPv6"
 
 sed -i 's/^ipv6 = .*/ipv6 = false/' "$CONFIG_FILE"
 grep -q '^ipv6 = false' "$CONFIG_FILE" || sed -i '/^\[network\]/a ipv6 = false' "$CONFIG_FILE"
@@ -95,7 +97,7 @@ sed -i 's/^listen_addr_ipv6 = "::"/#listen_addr_ipv6 = "::"/' "$CONFIG_FILE"
 sed -i '/^\[\[server\.listeners\]\]$/{N;s|^\[\[server\.listeners\]\]\nip = "::"|#[[server.listeners]]\n#ip = "::"|;}' "$CONFIG_FILE"
 
 # Уменьшение таймаутов клиентов
-echo Уменьшение таймаутов клиентов
+echo "Уменьшение таймаутов клиентов"
 
 sed -i 's/^client_keepalive = .*/client_keepalive = 15/' "$CONFIG_FILE"
 grep -q '^client_keepalive' "$CONFIG_FILE" || sed -i '/^\[timeouts\]/a client_keepalive = 15' "$CONFIG_FILE"
@@ -103,13 +105,13 @@ sed -i 's/^client_ack = .*/client_ack = 60/' "$CONFIG_FILE"
 grep -q '^client_ack' "$CONFIG_FILE" || sed -i '/^\[timeouts\]/a client_ack = 60' "$CONFIG_FILE"
 
 # Тихое логирование
-echo Тихое логирование
+echo "Тихое логирование"
 
 sed -i 's/^log_level = .*/log_level = "silent"/' "$CONFIG_FILE"
 grep -q '^log_level = "silent"' "$CONFIG_FILE" || sed -i '/^\[general\]/a log_level = "silent"' "$CONFIG_FILE"
 
 # Отключение телеметрии
-echo Отключение телеметрии
+echo "Отключение телеметрии"
 
 grep -q '^\[general\.telemetry\]' "$CONFIG_FILE" || echo -e '\n[general.telemetry]\n' >> "$CONFIG_FILE"
 sed -i 's/^core_enabled = .*/core_enabled = false/' "$CONFIG_FILE"
@@ -118,26 +120,26 @@ sed -i 's/^user_enabled = .*/user_enabled = false/' "$CONFIG_FILE"
 grep -q '^user_enabled' "$CONFIG_FILE" || sed -i '/^\[general\.telemetry\]/a user_enabled = false' "$CONFIG_FILE"
 
 # Отключение STUN
-echo Отключение STUN
+echo "Отключение STUN"
 
 sed -i 's/^stun_use = .*/stun_use = false/' "$CONFIG_FILE"
 grep -q '^stun_use' "$CONFIG_FILE" || sed -i '/^\[network\]/a stun_use = false' "$CONFIG_FILE"
 
 # Лимит подключений
-echo Лимит подключений
+echo "Лимит подключений"
 
 sed -i "s/^max_connections = .*/max_connections = $MAX_CONNECTIONS/" "$CONFIG_FILE"
 grep -q '^max_connections' "$CONFIG_FILE" || sed -i "/^\[server\]/a max_connections = $MAX_CONNECTIONS" "$CONFIG_FILE"
 
 # Отключение WEB-API
-echo Отключение WEB-API
+echo "Отключение WEB-API"
 
 grep -q '^\[server\.api\]' "$CONFIG_FILE" || echo -e '\n[server.api]\n' >> "$CONFIG_FILE"
 sed -i '/^\[server\.api\]/,/^\[/{s/^enabled *=.*/enabled = false/}' "$CONFIG_FILE"
 grep -q '^enabled = false' "$CONFIG_FILE" || sed -i '/^\[server\.api\]/a enabled = false' "$CONFIG_FILE"
 
 # Уменьшение буферов в BUF_DIVISOR раз
-echo Уменьшение буферов в BUF_DIVISOR раз
+echo "Уменьшение буферов в $BUF_DIVISOR раз"
 
 sed -i "s/^direct_relay_copy_buf_c2s_bytes = .*/direct_relay_copy_buf_c2s_bytes = $BUF_C2S/" "$CONFIG_FILE"
 grep -q '^direct_relay_copy_buf_c2s_bytes' "$CONFIG_FILE" || sed -i "/^\[general\]/a direct_relay_copy_buf_c2s_bytes = $BUF_C2S" "$CONFIG_FILE"
@@ -147,12 +149,12 @@ sed -i "s/^crypto_pending_buffer = .*/crypto_pending_buffer = $BUF_CRYPTO/" "$CO
 grep -q '^crypto_pending_buffer' "$CONFIG_FILE" || sed -i "/^\[general\]/a crypto_pending_buffer = $BUF_CRYPTO" "$CONFIG_FILE"
 
 # Рабочая директория telemt
-echo Рабочая директория telemt /var/lib/telemt
+echo "Рабочая директория telemt /var/lib/telemt"
 
 mkdir -p /var/lib/telemt
 
 # Запуск сервиса
-echo Запуск сервиса
+echo "Запуск сервиса"
 systemctl restart telemt
 
 # Ссылка на прокси-сервер
